5d69a915345d8314aa9159c8e02777c3c06ff506
tagei6422 <735765574@qq.com>
2022-05-10 11:00:46 +0800

    first

---
+++ src/main/java/com/zhj6422/springsecuritytest/SpringSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhj6422.springsecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zhj6422/springsecuritytest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhj6422.springsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
